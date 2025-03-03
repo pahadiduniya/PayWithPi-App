e538ad22440e4766a3699e145765c4d8086af4d2
pahadiduniya <pahadiduniya02@gmail.com>
2025-03-03 19:15:19 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PayWithPi from "./PayWithPi";
+import "./index.css"; // TailwindCSS setup
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PayWithPi />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
